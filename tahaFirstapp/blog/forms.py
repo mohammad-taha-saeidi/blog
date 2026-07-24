cdce0f858c65ddecf33090de726c54f108f30b38
taha <taha@gmail.com>
2026-07-24 09:25:50 +0330

    add forms.py and create ticket form

---
+++ tahaFirstapp/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+
+#ticket form
+class TicketForm(forms.ModelForm):
+    SUBJECT_CHOICES = (
+    ('گزارش', 'گزارش'),
+    ('انتقاد', 'انتقاد'),
+    ('پیشنهاد', 'پیشنهاد'),
+    )
+    message = forms.CharField(widget=forms.Textarea , required=True)
+    name = forms.CharField(max_length=250 , required=True)
+    email = forms.CharField(max_length=250)
+    phone = forms.CharField(max_length=11 , required=True)
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
+
