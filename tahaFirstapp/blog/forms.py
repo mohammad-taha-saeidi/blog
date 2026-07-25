30a72bad80522714bddae8dd43af7550fa66f83e
taha-saeidi <m.t.saeidi86@gmail.com>
2026-07-26 00:12:50 +0330

    forms update(vlesn_phone validations add)

--- tahaFirstapp/blog/forms.py
+++ tahaFirstapp/blog/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from .models import *
 
@@ -14,4 +15,13 @@ class TicketForm(forms.Form):
     email = forms.CharField(max_length=250)
     phone = forms.CharField(max_length=11 , required=True)
     subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
-
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if phone:
+            if not phone.isnumeric():
+                raise forms.ValidationError("شماره شما به صورت عددی وارد نشده است ")
+            elif len(phone) != 11:
+                raise forms.ValidationError("مقدار وارد شده برای شماره تلفن صحیح نمیباشد (11) کاراکتر!!")
+            else:
+                return phone
+        return None
