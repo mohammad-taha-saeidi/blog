eea14478a43187c6421a610a1fba17255b03db67
taha-saeidi <m.t.saeidi86@gmail.com>
2026-07-28 13:03:42 +0330

    create total_ command on blog_tags

--- tahaFirstapp/blog/templatetags/blog_tags.py
+++ tahaFirstapp/blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
-from ..models import Post
+from ..models import Post,Comment
 register = template.Library()
 @register.simple_tag(name = "tp")
 def total_post():
     return Post.Published_Manager.count()
+@register.simple_tag(name = "tc")
+def total_comments():
+    return Comment.objects.count()
