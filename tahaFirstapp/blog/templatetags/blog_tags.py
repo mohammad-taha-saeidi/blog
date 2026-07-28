17cffaeb024176f6f08b6646dad82b2534e0c642
taha-saeidi <m.t.saeidi86@gmail.com>
2026-07-28 12:49:01 +0330

    bog fix tp tag in blog_tags file

--- tahaFirstapp/blog/templatetags/blog_tags.py
+++ tahaFirstapp/blog/templatetags/blog_tags.py
@@ -3,4 +3,4 @@ from ..models import Post
 register = template.Library()
 @register.simple_tag(name = "tp")
 def total_post():
-    return Post.published.count()
+    return Post.Published_Manager.count()
