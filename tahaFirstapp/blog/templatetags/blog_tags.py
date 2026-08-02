29388164508e845847abf1a35a34a4ad027ab254
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-02 14:27:32 +0330

    update blog tmp-tags

--- tahaFirstapp/blog/templatetags/blog_tags.py
+++ tahaFirstapp/blog/templatetags/blog_tags.py
@@ -1,3 +1,5 @@
+from unicodedata import name
+
 from django import template
 from ..models import Post, Comment
 
@@ -23,12 +25,18 @@ def last_post():
 #     for post in Post.Published_Manager.all():
 #         i = post
 #         if post.comment.count()>
-@register.inclusion_tag("partials/latest_posts.html" , name="lpt")
+@register.inclusion_tag("partials/latest_posts.html" ,name="lpt")
 def latest_posts(count=5):
     l_post = Post.Published_Manager.order_by('-published')[:count]
     context = {
         'l_post': l_post,
     }
     return context
-
-
+#
+@register.inclusion_tag("partials/count_post.html" ,name="cp")
+def latest_posts(count=5):
+    count_post = Post.Published_Manager.order_by('-published')[:count].count()
+    context = {
+        'count_post': count_post,
+    }
+    return context
