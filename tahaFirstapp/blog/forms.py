a45773c5de297a96b314f4883c679ddcd8a8e0dd
taha-saeidi <m.t.saeidi86@gmail.com>
2026-08-03 12:02:19 +0330

    create Post form

--- tahaFirstapp/blog/forms.py
+++ tahaFirstapp/blog/forms.py
@@ -4,6 +4,10 @@ from django import forms
 from .models import *
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'description', 'slug', 'status',)
 #ticket form
 class TicketForm(forms.Form):
     SUBJECT_CHOICES = (
